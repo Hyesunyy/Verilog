fcb9c564f3cd7a6c8627202c8e59a5416f7c2479
Hyesunyy <97095192+Hyesunyy@users.noreply.github.com>
2024-10-07 19:04:02 +0900

    Add files via upload
    4bit 가산기

--- my_adder.v
+++ my_adder.v
@@ -21,18 +21,20 @@
 
 
 module my_adder(
-    input [1:0] A,
-    input [1:0] B,
-    output [1:0] S,
+    input [3:0] A,
+    input [3:0] B,
+    output  [3:0] S,
     output Co
     );
     
-    wire [1:0] Ca;
-    assign Co= Ca[1];
+    wire [3:0] Ca;
+    assign Co= Ca[3];
    
     
   My_fadder fa0(.A(A[0]), .B(B[0]),.Ci(1'b0), .S(S[0]),.Co(Ca[0]));  
   My_fadder fa1(.A(A[1]), .B(B[1]),.Ci(Ca[0]),.S(S[1]),.Co(Ca[1]));
+  My_fadder fa2(.A(A[2]), .B(B[2]),.Ci(Ca[1]), .S(S[2]),.Co(Ca[2]));  
+  My_fadder fa3(.A(A[3]), .B(B[3]),.Ci(Ca[2]),.S(S[3]),.Co(Ca[3]));
   
   
     
